2fb441d2b64a8e1cc43cb280c01c1c28a08cbfc8
liambloom <liamrbloom@gmail.com>
2018-12-09 20:18:32 -0500

    Wow, It takes a lot of stuff to make this work!

--- Countdown/js/countdown2.js
+++ Countdown/js/countdown2.js
@@ -1,4 +1,5 @@
 function countdown2(yt, mot, dt, ht,  mt, st, ut, until, message, background) {
+	var yfinder = new Date().getFullYear() + 1
 	var c = new Date(yt, mot, dt, ht, mt, st, ut).getTime();
 	var n = new Date().getTime();
 	var t = c - n;
@@ -42,5 +43,6 @@ function countdown2(yt, mot, dt, ht,  mt, st, ut, until, message, background) {
 		document.getElementById("un").innerHTML = u
 		document.getElementById("sl").innerHTML = "seconds"	
 		document.getElementById("to").innerHTML = until //new Date().getFullYear() + 1
+		console.log(yfinder, yfinder - 1)
 	}
 }
