274f0bf346642b13e30fef9972122cd1b2588895
liambloom <liamrbloom@gmail.com>
2018-12-23 12:01:13 -0500

    Deleted Comments

--- Countdown/js/Readable.js
+++ Countdown/js/Readable.js
@@ -1,22 +1,15 @@
 function Readable(theme) {
-	//let root = document.documentElement
 	switch(theme){
 		case '2':
-			//document.getElementById('cssCode').setAttribute('href', './css/css2.css');
-			//root.style.setProperty("--primary", "#70dfbf")
 			cssVarChange("#70dfbf", "#8cc4e0", "#3f1fcf")
 			break;
 		case '3':
-			//document.getElementById('cssCode').setAttribute('href', './css/css3.css');
 			cssVarChange("#3e2b18", "#452d13", "#f4f6f5")
 			break;
 		case 'Christmas':
-			//document.getElementById('cssCode').setAttribute('href', './css/cssC.css');
 			cssVarChange("#28beb4", "#24877e", "#0f524d")
 			break;
 		default:
-			//document.getElementById('cssCode').setAttribute('href', './css/css1.css');
 			cssVarChange("#EE2277", "#a7145d", "#0d1532")
 	}
-	//alert("Readable Ran")
 }
