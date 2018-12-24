3253daa8a4a0875cfc50db3d5ed987f23456e40f
liambloom <liamrbloom@gmail.com>
2018-12-23 22:41:43 -0500

    Added Semicolins

--- Countdown/js/makeDropdown.js
+++ Countdown/js/makeDropdown.js
@@ -1,12 +1,12 @@
 function makeDropdown(menu, start, end, pl) {
 	for (var i=start; i<=end; i++){
-		var o = document.createElement("option")
+		var o = document.createElement("option");
 		if (i.toString().length < pl){
-			i = "0" + i
+			i = "0" + i;
 		}
-		o.text = i
-		o.value = i
-		document.getElementById(menu).add(o)
-		i = Number(i)
+		o.text = i;
+		o.value = i;
+		document.getElementById(menu).add(o);
+		i = Number(i);
 	}
 }
