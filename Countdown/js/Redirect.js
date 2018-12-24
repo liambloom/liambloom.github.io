6e55198b63d0176ff91f79532b5b0e0c081dbcc5
liambloom <liamrbloom@gmail.com>
2018-12-23 22:33:46 -0500

    Removed else in Redirect()
     the else cleared links, which weren't always the problem

--- Countdown/js/Redirect.js
+++ Countdown/js/Redirect.js
@@ -1,7 +1,5 @@
 function Redirect(error){
 	if (window.confirm('This version of the countdown is not supported in your browser. You will be redirected to an earlier version of the countdown program. I apologise for any inconvenience.\n' + error)){
 		window.location.assign("https://liambloom.github.io/NewYearCountdown2.html");
-	}else{
-		document.getElementById("Labels").innerHTML = "";
 	}
 }
