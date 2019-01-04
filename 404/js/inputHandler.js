4c21c435893364ed4233a5dd1b8ad4061fa8e9b4
liambloom <liamrbloom@gmail.com>
2019-01-03 21:56:22 -0500

    More Infrastructure
    It doesn't do anything special yet, but the infrastructure to make it do that is getting larger

--- 404/js/inputHandler.js
+++ 404/js/inputHandler.js
@@ -1,4 +1,14 @@
 function inputHandler() {
-	$("pageText").innerHTML = $("pageText").innerHTML + "> " + $("textbox").value + "<br>";
-	$("textbox").value = ""
+	function addToPageText(whatToAdd) {
+		$("pageText").innerHTML = $("pageText").innerHTML + stateinfo.bullet + whatToAdd + "<br>";
+	}
+	addToPageText($("textbox").value)
+	$("textbox").value = stateinfo.textboxDefault
+	var output
+
+
+	//this code stays at the end
+	if (output != undefined){
+		addToPageText(output)
+	}
 }
