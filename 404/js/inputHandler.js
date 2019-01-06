0d7a9cb0e1f472a1fd4f584e7bfdcdfbfb31dce8
liambloom <liamrbloom@gmail.com>
2019-01-05 23:18:01 -0500

    That was not what I meant to do

--- 404/js/inputHandler.js
+++ 404/js/inputHandler.js
@@ -1,37 +1,180 @@
 function inputHandler() {
 	function addToPageText(whatToAdd, bullet) {
-		if (bullet == undefined) {bullet = stateinfo.bullet}
-		$("pageText").innerHTML += /*$("pageText").innerHTML + */bullet + whatToAdd + "<br>";
+		if (bullet == undefined) {bullet = ">"}
+		$("pageText").innerHTML += bullet + " " + whatToAdd + "<br>";
+	}
+	function strToBool(str) {
+		//console.log("str==" + str)
+		if (str == "true") {
+			return true
+		}
+		else if (str == "false") {
+			return false
+		}
+		else {
+			return undefined
+		}
 	}
 	var input = $("textbox").value
 	var output
-	var cheats = {
-		unblocked: {
-			set enable(setTo) {
-				stateinfo.cheatsEnabled = setTo//this does not work if it's false, only true
-				if (setTo == true) {//now this doesn't work either :(
+	/*var cheats = {
+		u: {
+			set enabled(setTo) {
+				var v = strToBool(setTo)
+				if (v == undefined) {
+					v = stateinfo.cheatsEnabled
+				}
+				stateinfo.cheatsEnabled = v
+				//console.log("setTo==" + setTo)
+				//console.log("v==" + v)
+				if (v) {
 					output = "Cheats Enabled"
 				}
-				else if (setTo == false) {//this does not work
+				else if (!v) {
 					output = "Cheats Disabled"
 				}
 				else {
-					output = "Error: " + this + "could not be set to" + setTo
+					output = "Error: " + this + " could not be set to " + v
 				}
+			},
+			help: function initiateHelp() {
+				stateinfo.stateChange = "help"
+				output = "What would you like help with: cheats or games?"
+				//console.log("hi")
+				//this.enabled = "true"
+			},
+			enable_cheats: function chEn() {
+				//console.log(this)
+				this.enabled = "true"
+			},
+			disable_cheats: function chDi() {
+				this.enabled = "false"
 			}
+		},
+		test: 10,
+		ftest: function testFun() {
+			//console.log(inputhere)
+			output = "ftest ran"
+		},
+		get theThing() {
+			stateinfo.state
 		}
-	}
-	$("textbox").value = stateinfo.textboxDefault
+	}*/
+	//if (input == "#State Info") {console.log(stateinfo.state)}
 	if (input.includes("/")){
-		if (stateinfo.cheatsEnabled || input.includes("/unblocked")) {
-			var cheatHandler = input.replace(" ", "").split("=")
-			cheatHandler[0] = cheatHandler[0].split(".")
+		var cheats = {
+			u: {
+				set enabled(setTo) {
+					var v = strToBool(setTo)
+					if (v == undefined) {
+						v = stateinfo.cheatsEnabled
+					}
+					stateinfo.cheatsEnabled = v
+					//console.log("setTo==" + setTo)
+					//console.log("v==" + v)
+					if (v) {
+						output = "Cheats Enabled"
+					}
+					else if (!v) {
+						output = "Cheats Disabled"
+					}
+					else {
+						output = "Error: " + this + " could not be set to " + v
+					}
+				},
+				help: function initiateHelp() {
+					stateinfo.stateChange = "help"
+					output = "What would you like help with: cheats or games?"
+					//console.log("hi")
+					//this.enabled = "true"
+				},
+				enable_cheats: function chEn() {
+					//console.log(this)
+					this.enabled = "true"
+				},
+				disable_cheats: function chDi() {
+					this.enabled = "false"
+				}
+			},
+			test: 10,
+			ftest: function testFun() {
+				//console.log(inputhere)
+				output = "ftest ran"
+			},
+			get theThing() {
+				stateinfo.state
+			}	
+		}
+		if (stateinfo.cheatsEnabled || input.includes("/u")) {
+			//var cheatHandler = input.replace(" ", "").split("=")
+			for (var cheatHandler = input.toLowerCase(); cheatHandler.includes(" ");) {
+				cheatHandler = cheatHandler.replace(" ", "")
+			}
+			/*cheatHandler = cheatHandler.split("=")
+			cheatHandler[0] = cheatHandler[0].split(".")*/
 			try{
-				cheats[cheatHandler[0][0].replace("/", "")][cheatHandler[0][1]] = cheatHandler[1]//this needs to work even if cheat does not fit that syntax
+				if (cheatHandler.includes("=")){
+					cheatHandler = cheatHandler.split("=")
+					cheatHandler[0] = cheatHandler[0].split(".")
+					switch (cheatHandler[0].length) {
+						case 1:
+							cheats[cheatHandler[0][0].replace("/", "")] = cheatHandler[1]
+							break;
+						case 2:
+							cheats[cheatHandler[0][0].replace("/", "")][cheatHandler[0][1]] = cheatHandler[1]
+							break;
+					}
+					//console.log("=")
+				}
+				else if (cheatHandler.includes("/get")) {
+					cheatHandler = cheatHandler.split(".")
+					switch (cheatHandler.length) {
+						case 1:
+							output = cheats[cheatHandler[0].replace("/get", "")].toString()
+							//console.log(cheats[cheatHandler[0].replace("/get", "")].toString())
+							break;
+						case 2:
+							output = cheats[cheatHandler[0].replace("/get", "")][cheatHandler[1]].toString()
+							break;
+					}
+					//console.log("get")
+				}
+				else if (cheatHandler.includes("(")) {// untested
+					cheatHandler = cheatHandler.split("(")
+					//cheatHandler = cheatHandler.split(")")
+					cheatHandler[0] = cheatHandler[0].split(".")
+					switch (cheatHandler[0].length) {
+						case 1:
+							cheats[cheatHandler[0][0].replace("/", "")](cheatHandler[1].replace(")", ""))
+							break;
+						case 2:
+							cheats[cheatHandler[0][0].replace("/", "")][cheatHandler[0][1]](cheatHandler[1].replace(")", ""))
+							break;
+					}
+					//console.log("()")
+				}
+				else {
+					cheatHandler = cheatHandler.split(".")
+					switch (cheatHandler.length) {
+						case 1:
+							cheats[cheatHandler[0].replace("/", "")]()
+							break;
+						case 2:
+							//console.log("test1")
+							//console.log("handle1:"+cheatHandler[0] + " / handle2: "+cheatHandler[1]);
+							cheats[cheatHandler[0].replace("/", "")][cheatHandler[1]]()
+							//console.log("test2")
+							//cheats["u"]["help_Me_Please"]()
+							break;
+					}
+				}
+				
+				//cheats[cheatHandler[0][0].replace("/", "")][cheatHandler[0][1]] = cheatHandler[1]//this needs to work even if cheat does not fit the syntax /something.something = something
 			}
 			catch(err){
+				//console.log(cheatHandler[0].replace("/", "") + " " + [cheatHandler[1]])
 				output = "That cheat does not exist<br>\
-				Try \"/unblocked.help\""
+				Try \"/u.help\"<br>" + err 
 			}
 		}
 		/*else if (input == "/Enable Cheats") {
@@ -40,20 +183,23 @@ function inputHandler() {
 		}*/
 		else {
 			output = "Error: Cheats are not Enabled<br>\
-				To enable cheats type &quot;/unblocked.enable = true&quot;"
+				To enable cheats type &quot;/u.enable_cheats&quot;"
 		}
 	}
 
 
 	//this code stays at the end
+	$("textbox").value = stateinfo.textboxDefault
 	addToPageText(input)
 	if (output != undefined){
-		if (output.includes("<br>")) {
-			//console.log("output includes <br>")
+		/*if (output.includes("<br>")) {
 			output = output.replace("<br>", "<br><span class='indent'>")
 			output += "</span>"
-		}
-		//console.log(output)
-		addToPageText(output, "< ")
+		}*/
+		//for (var i = 0; i <= output.match(/<br>/g).length; i++) {
+			output = output.replace(/<br>/g, "<br><span class='indent'>")
+			output += "</span>"
+		//}
+		addToPageText(output, "<")
 	}
 }
