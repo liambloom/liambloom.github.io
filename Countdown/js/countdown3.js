2a925107d3f992c3ca1eedb78b9d4c1835f45b90
liambloom <liamrbloom@gmail.com>
2019-03-10 22:00:46 -0400

    New version of countdown()

--- Countdown/js/countdown3.js
+++ Countdown/js/countdown3.js
@@ -1,13 +1,16 @@
 //jshint esversion:6
-const countdown3 = (c = new Date(1970, 0, 1, 0, 0, 0, 0).getTime(), until = "Something Happens", message = "Something Happened", background = 'url("./img/fireworks.gif")') => {
-	//var c = new Date(yt, mot, dt, ht, mt, st, ut).getTime();
+const countdown3 = t => {
+	//throw kill;
+	//console.log("countdown3 ran");
+	/*var c = new Date(yt, mot, dt, ht, mt, st, ut).getTime();
 	var n = new Date().getTime();
-	var t = c - n;
+	var t = c - n;*/
 	var d = Math.floor(t / 86400000).toString();
 	var h = Math.floor((t % 86400000) / 3600000).toString();
 	var m = Math.floor((t % 3600000) / 60000).toString();
-	var s = Math.floor((t % 60000) / 1000).toString();
-	var u = "." + Math.floor(t % 1000).toString();
+	var s = (Math.floor((t % 60000) / 1000)).toString();//why does seconds start 1 lower than it shoud and wait a second to catch up at the start of the program
+	//console.log(`t=${t} s=${s}`);
+	/*var u = "." + Math.floor(t % 1000).toString();
 	while (u.length < 4) {
 		u = u + "0";
 	}
@@ -18,47 +21,53 @@ const countdown3 = (c = new Date(1970, 0, 1, 0, 0, 0, 0).getTime(), until = "Som
 			Readable();
 		}
 		clearInterval(count);
-	} else {
-		set(dn, d, () => {
-			if (d > 0) {
-				document.getElementById("dn").innerHTML = d;
-				document.getElementById("dl").innerHTML = "day";
-				if (d > 1) {
-					document.getElementById("dl").innerHTML += "s";
-				}
-			} else {
-				document.getElementById("dn").innerHTML = "";
-				document.getElementById("dl").innerHTML = "";
-			}
-		});
-		set(hn, h, () => {
-			if (h > 0) {
-				document.getElementById("hn").innerHTML = h;
-				document.getElementById("hl").innerHTML = "hour";
-				if (h > 1) {
-					document.getElementById("hl").innerHTML += "s";
-				}
-			} else {
-				document.getElementById("hn").innerHTML = "";
-				document.getElementById("hl").innerHTML = "";
-			}
-		});
-		set(mn, m, () => {
-			if (m > 0) {
-				document.getElementById("mn").innerHTML = m;
-				document.getElementById("ml").innerHTML = "minute";
-				if (m > 1) {
-					document.getElementById("ml").innerHTML = "s";
-				}
-			} else {
-				document.getElementById("mn").innerHTML = "";
-				document.getElementById("ml").innerHTML = "";
-			}
-		});
-		set(sn, s, () => {document.getElementById("sn").innerHTML = s;});
-		set(sl, "seconds", () => {
-			document.getElementById("sl").innerHTML = "seconds";
-			document.getElementById("to").innerHTML = until;
-		});
 	}
+	else {*/
+	//if (s < 0) throw "something broke";
+	set("dn", d, () => {
+		if (d > 0) {
+			document.getElementById("dn").innerHTML = d;
+			document.getElementById("dl").innerHTML = "day";
+			if (d > 1) {
+				document.getElementById("dl").innerHTML += "s";
+			}
+		}
+		else {
+			document.getElementById("dn").innerHTML = "";
+			document.getElementById("dl").innerHTML = "";
+		}
+	});
+	set("hn", h, () => {
+		if (h > 0) {
+			document.getElementById("hn").innerHTML = h;
+			document.getElementById("hl").innerHTML = "hour";
+			if (h > 1) {
+				document.getElementById("hl").innerHTML += "s";
+			}
+		}
+		else {
+			document.getElementById("hn").innerHTML = "";
+			document.getElementById("hl").innerHTML = "";
+		}
+	});
+	set("mn", m, () => {
+		if (m > 0) {
+			document.getElementById("mn").innerHTML = m;
+			document.getElementById("ml").innerHTML = "minute";
+			if (m > 1) {
+				document.getElementById("ml").innerHTML += "s";
+			}
+		}
+		else {
+			document.getElementById("mn").innerHTML = "";
+			document.getElementById("ml").innerHTML = "";
+		}
+	});
+	//console.log(t, " ", s);
+	set("sn", s, () => {document.getElementById("sn").innerHTML = s;/* console.log(t, " ", s);*/});
+	/*set(sl, "seconds", () => {
+		document.getElementById("sl").innerHTML = "seconds";
+		document.getElementById("to").innerHTML = until;
+	});
+	}*/
 };
--- Countdown/js/mscountdown.js
+++ Countdown/js/mscountdown.js
@@ -1,8 +1,25 @@
 //jshint esversion:6
-const msCountdown = (c, until, message, background) => {
-	document.getElementById("un").innerHTML = u;
-	var n = new Date().getTime();
-	var t = c - n;
-	var u = Math.floor(t % 1000);
-	if (u === 0) countdown3(c, until, message, background);
+const msCountdown = (c, atEnd = () => end()) => {
+	//console.log("msCoutndown ran");
+	let n = new Date().getTime();
+	let t = c - n;
+	if (t <= 0) {
+		//console.log(t);
+		//console.log("end");
+		atEnd();
+		return;
+	}
+	else {
+		//console.log(t);
+		let u = Math.floor(t % 1000);
+		//console.log(c);
+		//throw "end";
+		//console.log(u);
+		if (u <= 35 && t >=1000) countdown3(t - 1000);
+		u = "." + u.toString();
+		while (u.length < 4) {
+			u = u + "0";
+		}
+		document.getElementById("un").innerHTML = u;
+	}
 };
