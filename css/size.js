de7ac863395399e2111a1e49ea4960360241d4ed
liambloom <liamrbloom@gmail.com>
2019-03-18 22:18:23 -0400

    Update size.js

--- css/size.js
+++ css/size.js
@@ -2,5 +2,7 @@
 const root = document.documentElement;//imposible to not get this first
 window.onresize = () => {
 	//console.log(-document.getElementsByTagName("h1")[0].clientHeight);
-	root.style.setProperty("--size", -document.getElementsByTagName("h1")[0].clientHeight - 8 + "px");
+	root.style.setProperty("--size", -document.getElementsByTagName("h1")[0].clientHeight - 5 + "px");
+	root.style.setProperty("--angle", Math.atan(document.getElementsByTagName("header")[0].clientHeight/window.innerWidth) + "rad");
 };
+window.onload = window.onresize;
